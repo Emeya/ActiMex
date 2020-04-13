a3e9f9f9256391ba42fc92f75e0a3d1e41c56fdb
Manuel Soberanis <emeyappdeveloper_BK@hotmail.com>
2020-04-13 11:19:28 -0500

    Setup ready to load the app without storyboard. Emeya

--- ActiMex/ViewControllers/MainViewController.swift
+++ ActiMex/ViewControllers/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  ActiMex
 //
 //  Created by Manuel Soberanis on 13/04/20.
@@ -8,13 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        self.view.backgroundColor = .red
     }
-
-
+    
+    
 }
-
